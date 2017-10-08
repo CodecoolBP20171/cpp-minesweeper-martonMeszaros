ac26b349b56b6cd63b88ca576f11a6b9b33b0d53
martonMeszaros <marton.m95@gmail.com>
2017-10-08 19:23:31 +0200

    Step 1 and Step 3

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
+#include <cmath>
 
 namespace {
     class Minesweeper {
@@ -26,12 +29,23 @@ namespace {
         }
 
         void printTable() const {
-            // step 3 goes here
+            for (int i = 0; i < width * height; ++i) {
+                std::cout << table[i];
+                if (i % width == width -1) {
+                    std::cout << "\n";
+                }
+            }
         }
 
     private:
         void fillTable() {
-            // step 1 goes here
+            srand(time(0));
+            // The chance for a bomb to spawn is different for
+            // every Minesweeper object, between 10-15%.
+            int mineChance = 10 + (rand() % 6);
+            for (int i = 0; i < width * height; ++i) {
+                *(table+i) = (rand() % 100 < mineChance) ? '*' : '.';
+            }
         }
 
         const size_t width, height;
@@ -41,10 +55,10 @@ namespace {
 
 int main() {
     try {
-        Minesweeper ms(100, 50);
+        Minesweeper ms(25, 10);
         ms.printTable();
-        ms.countNeighbours();
-        ms.printTable();
+//        ms.countNeighbours();
+//        ms.printTable();
     } catch (const std::bad_alloc &e) {
         std::cerr << "Couldn't allocate enough memory for minesweeper table" << std::endl;
         return EXIT_FAILURE;
